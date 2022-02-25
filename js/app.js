b8e959d45eab2e34897239ec33359c5b9d25af7d
Diego Mateus <alejandromateus31@outlook.com>
2022-02-24 19:58:18 -0500

    selectores

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-console.log("se inicializa el projecto");
+console.log(document.querySelector('h3'));
+console.log(document.querySelector('.h3-danger'));
+console.log(document.querySelector('#parrafo'));
+console.log(document.getElementById('parrafo'));
+console.log("---");
+console.log(document.querySelectorAll('h3'));
+
+console.log("Modificar");
+const parrafo = document.querySelector("#parrafo");
+parrafo.textContent = 'texto desde js';
+//parrafo.innerHTML = '<b>texto desde vanilla</b>';
+parrafo.classList.add('h3-danger', 'my-2');
